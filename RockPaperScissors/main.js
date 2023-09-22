721d9ad108bf149badc721a30240d4d4f57d9141
RiteshChepuri <riteshchepuri0@gmail.com>
2023-09-22 11:59:31 +0530

    corrected the logic

--- RockPaperScissors/main.js
+++ RockPaperScissors/main.js
@@ -4,40 +4,41 @@ let wins = 0;
 let losses = 0;
 let draws = 0;
 
-while (true){
-  const playerChoice = prompt("Enter Rock,Paper,Scissor(or q to quit)");
-  if (playerChoice.toLowerCase() === "q"){
+while (true) {
+  const playerChoice = prompt("Enter Rock,Paper,Scissor(or q to quit) ");
+  if (playerChoice.toLowerCase() === "q") {
     break;
   }
 
-  if (playerChoice !== "rock" &&
+  if (
+    playerChoice !== "rock" &&
     playerChoice !== "scissor" &&
-    playerChoice !== "paper"){
+    playerChoice !== "paper"
+  ) {
     prompt("Please enter a valid entry");
     continue;
   }
 
-
-  const choices = ["rock","scissor","paper"];
-  const randomIndex = Math.round(Math.random()* 2);
+  const choices = ["rock", "scissor", "paper"];
+  const randomIndex = Math.round(Math.random() * 2);
   const computerChoice = choices[randomIndex];
 
-  console.log("Computer choice is ",computerChoice);
+  console.log("Computer choice is ", computerChoice);
 
-  if(computerChoice === playerChoice){
+  if (computerChoice === playerChoice) {
     console.log("draw");
-    draws ++;
-  }
-  else if ((playerChoice === "paper" && computerChoice === "rock") ||
+    draws++;
+  } else if (
+    (playerChoice === "paper" && computerChoice === "rock") ||
     (playerChoice === "scissor" && computerChoice === "paper") ||
-    (playerChoice === "scissor" && computerChoice === "paper")){
-      console.log("Player has won");
-      wins ++;
-    }else{
-      console.log("Player has lost");
-      losses ++;
-    }
+    (playerChoice === "rock" && computerChoice === "scissor")
+  ) {
+    console.log("Player has won");
+    wins++;
+  } else {
+    console.log("Player has lost");
+    losses++;
+  }
 }
 
-
-console.log("wins:",wins, "Losses:",losses,"Draws:",draws)
+console.log("wins:", wins, "Losses:", losses, "Draws:", draws);
